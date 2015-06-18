62f146bcab420288c2c993b87a3c42adb80eb8bd
M. Lenderink <martijnrules@gmail.com>
2015-06-18 14:14:33 +0200

    Ember naar 1.13.2 en Ember-data naar 1.13.0

--- addon/adapters/cobra-rest.js
+++ addon/adapters/cobra-rest.js
@@ -6,6 +6,16 @@ import DS from 'ember-data';
  **/
 export default DS.RESTAdapter.extend({
 
+	// FIXME: Tijdelijke fix ivm deprecation
+	shouldReloadAll: function() {
+		return true;
+	},
+
+	// FIXME: Tijdelijke fix ivm deprecation 
+	shouldBackgroundReloadRecord: function() {
+		return false;
+	},
+
 	headers: {},
 
 	ajaxOptions: function(url, type, hash) {
